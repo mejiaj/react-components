aa0a1a9cbb0b1f096964b34da407ac2b7820652d
James Mejia <james.mejia@bixal.com>
2025-06-20 11:43:15 -0500

    Use function declarations

--- stories/Page/Page.jsx
+++ stories/Page/Page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Header } from "../Header/Header";
 import "./page.css";
 
-export const Page = () => {
+export function Page() {
   const [user, setUser] = React.useState();
 
   return (
